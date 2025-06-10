7f6e4ed4dfcf5a7d7b7a8ddc2aff786f17aae7b7
alamdin stephen <>
2025-06-10 22:15:00 +0900

    Add LoginViewModel

---
+++ app/src/main/java/com/example/nanameue_code_test/ui/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.nanameue_code_test.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    fun login() {
+        // TODO: Implement login logic
+    }
+} 
