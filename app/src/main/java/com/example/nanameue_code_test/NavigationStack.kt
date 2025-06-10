8cd58bfc525758161d55fc76e10e9d119703bc8e
alamdin stephen <>
2025-06-10 23:48:49 +0900

    Add Login page and Sign up page navigation

--- app/src/main/java/com/example/nanameue_code_test/NavigationStack.kt
+++ app/src/main/java/com/example/nanameue_code_test/NavigationStack.kt
@@ -1,21 +1,56 @@
 package com.example.nanameue_code_test
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.nanameue_code_test.ui.login.LoginEvent
 import com.example.nanameue_code_test.ui.login.LoginScreen
+import com.example.nanameue_code_test.ui.login.LoginViewModel
+import com.example.nanameue_code_test.ui.sign_up.SignUpEvent
+import com.example.nanameue_code_test.ui.sign_up.SignUpScreen
+import com.example.nanameue_code_test.ui.sign_up.SignUpViewModel
 import com.example.nanameue_code_test.ui.timeline.TimelineScreen
+import kotlinx.coroutines.flow.merge
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun NavigationStack() {
     val navController = rememberNavController()
+    val loginViewModel: LoginViewModel = koinViewModel()
+    val signUpViewModel: SignUpViewModel = koinViewModel()
+
+    LaunchedEffect(Unit) {
+        merge(
+            loginViewModel.navigationEvent,
+            signUpViewModel.navigationEvent
+        ).collect { event ->
+            when (event) {
+                is LoginEvent.NavigateToSignUp -> {
+                    navController.navigate(Screen.SignUp.route)
+                }
+
+                is LoginEvent.NavigateToTimeline -> {
+                    navController.navigate(Screen.Timeline.route) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                }
+
+                is SignUpEvent.NavigateBack -> {
+                    navController.navigateUp()
+                }
+            }
+        }
+    }
 
     NavHost(navController = navController, startDestination = Screen.Login.route) {
         composable(route = Screen.Login.route) {
-            LoginScreen()
+            LoginScreen(viewModel = loginViewModel)
         }
-
+        composable(route = Screen.SignUp.route) {
+            SignUpScreen(viewModel = signUpViewModel)
+        }
         composable(route = Screen.Timeline.route) {
             TimelineScreen()
         }
--- app/src/main/java/com/example/nanameue_code_test/di/AppModule.kt
+++ app/src/main/java/com/example/nanameue_code_test/di/AppModule.kt
@@ -1,11 +1,13 @@
 package com.example.nanameue_code_test.di
 
 import com.example.nanameue_code_test.ui.login.LoginViewModel
+import com.example.nanameue_code_test.ui.sign_up.SignUpViewModel
 import com.example.nanameue_code_test.ui.timeline.TimelineViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { LoginViewModel() }
+    viewModel { SignUpViewModel() }
     viewModel { TimelineViewModel() }
 }
