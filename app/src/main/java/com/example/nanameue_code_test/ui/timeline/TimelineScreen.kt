7ca86d231a48ae74944fc578401ac6daadf676c3
alamdin stephen <>
2025-06-10 22:17:11 +0900

    Add Timeline screen related files

---
+++ app/src/main/java/com/example/nanameue_code_test/ui/timeline/TimelineScreen.kt
@@ -0,0 +1,4 @@
+package com.example.nanameue_code_test.ui.timeline
+
+fun TimelineScreen() {
+}
---
+++ app/src/main/java/com/example/nanameue_code_test/ui/timeline/TimelineViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.nanameue_code_test.ui.timeline
+
+import androidx.lifecycle.ViewModel
+
+class TimelineViewModel: ViewModel() {
+    fun fetchTimeline() {
+        // TODO: Implement fetchTimeline logic
+    }
+}
