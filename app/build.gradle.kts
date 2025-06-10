6dc34e9537576da0f7e874ea19f5e84b2eda3a1d
alamdin stephen <>
2025-06-10 22:54:37 +0900

    Add Koin for di

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/example/nanameue_code_test/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.nanameue_code_test.di
+
+import com.example.nanameue_code_test.ui.login.LoginViewModel
+import com.example.nanameue_code_test.ui.timeline.TimelineViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    viewModel { LoginViewModel() }
+    viewModel { TimelineViewModel() }
+}
