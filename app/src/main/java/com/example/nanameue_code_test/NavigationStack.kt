9cd83fda78a0957e1087b95dc6b63aef4c115964
alamdin stephen <>
2025-06-12 23:27:07 +0900

    Update routes

--- app/src/main/java/com/example/nanameue_code_test/NavigationStack.kt
+++ app/src/main/java/com/example/nanameue_code_test/NavigationStack.kt
@@ -5,36 +5,72 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.nanameue_code_test.ui.auth.AuthViewModel
+import com.example.nanameue_code_test.ui.create_post.CreatePostEvent
+import com.example.nanameue_code_test.ui.create_post.CreatePostScreen
+import com.example.nanameue_code_test.ui.create_post.CreatePostViewModel
 import com.example.nanameue_code_test.ui.login.LoginEvent
 import com.example.nanameue_code_test.ui.login.LoginScreen
 import com.example.nanameue_code_test.ui.login.LoginViewModel
+import com.example.nanameue_code_test.ui.profile.ProfileEvent
+import com.example.nanameue_code_test.ui.profile.ProfileScreen
+import com.example.nanameue_code_test.ui.profile.ProfileViewModel
 import com.example.nanameue_code_test.ui.sign_up.SignUpEvent
 import com.example.nanameue_code_test.ui.sign_up.SignUpScreen
 import com.example.nanameue_code_test.ui.sign_up.SignUpViewModel
+import com.example.nanameue_code_test.ui.timeline.TimelineEvent
 import com.example.nanameue_code_test.ui.timeline.TimelineScreen
+import com.example.nanameue_code_test.ui.timeline.TimelineViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.merge
 import org.koin.androidx.compose.koinViewModel
+
 
 @Composable
 fun NavigationStack() {
     val navController = rememberNavController()
     val loginViewModel: LoginViewModel = koinViewModel()
     val signUpViewModel: SignUpViewModel = koinViewModel()
+    val timelineViewModel: TimelineViewModel = koinViewModel()
+    val profileViewModel: ProfileViewModel = koinViewModel()
+    val createPostViewModel: CreatePostViewModel = koinViewModel()
+    val authViewModel: AuthViewModel = koinViewModel()
 
     LaunchedEffect(Unit) {
         merge(
             loginViewModel.navigationEvent,
-            signUpViewModel.navigationEvent
-        ).collect { event ->
+            signUpViewModel.navigationEvent,
+            profileViewModel.navigationEvent,
+            timelineViewModel.navigationEvent,
+            createPostViewModel.navigationEvent
+        ).collectLatest { event ->
             when (event) {
                 is LoginEvent.NavigateToSignUp -> {
                     navController.navigate(Screen.SignUp.route)
                 }
 
-                is LoginEvent.NavigateToTimeline -> {
+                is ProfileEvent.NavigateToTimeline,
+                LoginEvent.NavigateToTimeline,
+                SignUpEvent.NavigateToTimeline,
+                CreatePostEvent.NavigateToTimeline
+                    -> {
                     navController.navigate(Screen.Timeline.route) {
-                        popUpTo(Screen.Login.route) { inclusive = true }
+                        popUpTo(0) { inclusive = true }
                     }
+                }
+
+                is ProfileEvent.NavigateToLogin -> {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                }
+
+                is TimelineEvent.NavigateToProfile -> {
+                    navController.navigate(Screen.Profile.route)
+                }
+
+                is TimelineEvent.NavigateToCreatePost -> {
+                    navController.navigate(Screen.CreatePost.route)
                 }
 
                 is SignUpEvent.NavigateBack -> {
@@ -46,13 +82,22 @@ fun NavigationStack() {
 
     NavHost(navController = navController, startDestination = Screen.Login.route) {
         composable(route = Screen.Login.route) {
-            LoginScreen(viewModel = loginViewModel)
+            LoginScreen(loginViewModel, authViewModel)
         }
         composable(route = Screen.SignUp.route) {
-            SignUpScreen(viewModel = signUpViewModel)
+            SignUpScreen(signUpViewModel, authViewModel)
         }
         composable(route = Screen.Timeline.route) {
-            TimelineScreen()
+            TimelineScreen(timelineViewModel)
         }
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(profileViewModel, authViewModel)
+        }
+        composable(route = Screen.CreatePost.route) {
+            CreatePostScreen(createPostViewModel, authViewModel)
+        }
+
     }
 }
+
+open class NavigationEvent {}
