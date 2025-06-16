23ec1837e1c64aa87d3113b14821a0543e3e5d40
alamdin stephen <>
2025-06-16 21:01:23 +0900

    Add compose preview for loading screen

--- app/src/main/java/com/example/nanameue_code_test/ui/common/FullScreenLoading.kt
+++ app/src/main/java/com/example/nanameue_code_test/ui/common/FullScreenLoading.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -55,3 +56,11 @@ fun FullScreenLoading() {
         }
     }
 }
+
+
+@Preview(showBackground = false)
+@Composable
+fun FullScreenLoadingPreview() {
+    FullScreenLoading()
+}
+
--- app/src/main/java/com/example/nanameue_code_test/ui/common/LoadingDialog.kt
+++ app/src/main/java/com/example/nanameue_code_test/ui/common/LoadingDialog.kt
@@ -16,11 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -65,4 +65,10 @@ fun LoadingDialog() {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingDialogPreview() {
+    LoadingDialog()
 }
